48cdf87644c8c9baed55c595156910738cc9e038
Haripreetam <haripreetam333@gmail.com>
2024-08-04 16:07:06 +0530

    [Haripreetam] ADD:python script for printing the difference between two sets

---
+++ set_problems/08_difference.py
@@ -0,0 +1,9 @@
+def set_difference(set1, set2):
+    return set1.difference(set2)
+
+# Example usage
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+difference = set_difference(set1, set2)
+print(difference)  # Output: {1}
+ 
