babc1355386f61cad2d5c22c2f111bd00632d624
Haripreetam <haripreetam333@gmail.com>
2024-08-04 11:47:44 +0530

    [Haripreetam] ADD:python script to find the frequency of the char

---
+++ String_problems/02_char_frequency.py
@@ -0,0 +1,10 @@
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+print(char_frequency("inefinfniawdfiied"))  
+
