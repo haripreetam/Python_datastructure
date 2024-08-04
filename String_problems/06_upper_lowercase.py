a93de09435beae363cbd3e916593e3251f372e27
Haripreetam <haripreetam333@gmail.com>
2024-08-04 12:15:01 +0530

    [Haripreetam] ADD:python script for finding longest word

---
+++ String_problems/06_upper_lowercase.py
@@ -0,0 +1,6 @@
+def display_user_input():
+    user_input = input("Enter a string: ")
+    print("The string in uppercase is: ", user_input.upper())
+    print("The string in lowercase is: ", user_input.lower())
+
+display_user_input()
