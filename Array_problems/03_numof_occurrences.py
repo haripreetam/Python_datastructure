12465949e1bc2f1218ca4a0234f4e677088b4687
Haripreetam <haripreetam333@gmail.com>
2024-08-04 16:32:51 +0530

    [Haripreetam] ADD:python script for finding the number of times an element has occured in an array

---
+++ Array_problems/03_numof_occurrences.py
@@ -0,0 +1,7 @@
+def count_occurrences(array, element):
+    return array.count(element)
+
+array = [1, 2, 3, 4, 2, 2, 5]
+element = 2
+occurrences = count_occurrences(array, element)
+print(occurrences)
