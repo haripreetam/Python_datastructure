32bb43489a68c5ffb83d7ade154d18b2e971e596
Haripreetam <haripreetam333@gmail.com>
2024-08-04 12:32:29 +0530

    [Haripreetam] ADD:python script for print the text before a specified char

---
+++ String_problems/08_last_part_before_char.py
@@ -0,0 +1,6 @@
+def last_part_before_char():
+    str1 = "https://www.w3resource.com/python-exercises/string"
+    print(str1.rsplit('.', 1)[0])
+#                       occurr)[flag
+last_part_before_char()
+
