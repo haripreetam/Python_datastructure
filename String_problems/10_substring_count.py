f99f9ae7b74a76673d023269407b761a0ffd4758
Haripreetam <haripreetam333@gmail.com>
2024-08-04 12:35:20 +0530

    [Haripreetam] ADD:python script for counting the number of occurrances of a substring

---
+++ String_problems/10_substring_count.py
@@ -0,0 +1,6 @@
+def counting_substring(text,sub):
+    return text.count(sub)
+
+text = "The quick brown fox jumps over the lazy dog."
+sub = "o"
+print(counting_substring("The quick brown fox jumps over the lazy dog.","o"))
